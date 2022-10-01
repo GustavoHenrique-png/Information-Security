a7cc4c2353f77e3cf37c1515f74d65b3c4f96042
Gustavo Henrique <ymooncake@gmail.com>
2022-09-30 22:22:04 -0300

    Update honepot.py

--- source/honepot.py
+++ source/honepot.py
@@ -1,2 +1,28 @@
-import socket
+from distutils.log import error
+import ipaddress
+from socket import *
 
+def honeyPot():
+    ipAdress = ""
+    port = 80
+
+    try:
+        getConnection = socket(AF_INET,SOCK_STREAM)
+        getConnection.bind((ipAdress,port))
+        getConnection.listen(10)
+        while True:
+            clientConnection,clientAdress = getConnection.accept()
+            print(clientAdress[0])
+            data = clientConnection.recv(1024)
+            print(data.decode('utf-8'))
+        
+    except error as identifier:
+        print("Error: " + identifier)
+    except KeyboardInterrupt as ky:
+        print("Error: " + ky)
+    finally:
+        getConnection.close()
+    getConnection.close()
+
+if __name__ == "__honeyPot__":
+    honeyPot()
