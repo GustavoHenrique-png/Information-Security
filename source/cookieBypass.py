8ebb9d3f0c0ff8ec26fe8d37edb05fd7b011486e
Gustavo Henrique <ymooncake@gmail.com>
2023-04-24 11:18:45 -0300

    Update cookieBypass.py

--- source/cookieBypass.py
+++ source/cookieBypass.py
@@ -1,7 +1,21 @@
 import requests
-data = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0", "Cookie": "cf_clearance=u34gBLtHQhaRMb9MYEF_gGH6tQL.zMoGfdRC9Vffr9s-1680725687-0-250"}
 
-requisition = requests.get("http://www.bancocn.com/", headers=data)
-print (requisition.status_code)
+print(r"""
+      /\
+ __   \/   __
+ \_\_\/\/_/_/
+   _\_\/_/_
+  __/_/\_\__
+ /_/ /\/\ \_\
+ Snow /\
+      \/
+""")
 
 
+data = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0","Cookie":"cf_clearance=UtdbThPq84pdOn0t.3Z7CXuRQd90c6.rmU36P7XnnnE-1680797523-0-250"}
+
+requisition = requests.get("http://www.bancocn.com/", headers= data)
+if (requisition.status_code ==200):
+    print("Access: granted\nStatus: ",requisition.status_code)
+else:
+     print("Access: denied\nStatus: ",requisition.status_code)
